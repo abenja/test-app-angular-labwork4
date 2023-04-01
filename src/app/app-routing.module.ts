cd76ff9b68dac4fcf1f3d506e789433b46fdf96c
abenja <73184325+abenja@users.noreply.github.com>
2023-04-01 20:23:40 +0300

    Initial commit, everything works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,36 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
+
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
+ 
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'loader',
+    loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( (m) => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToHome)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( (m) => m.RegisterPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( (m) => m.HomePageModule),
+    ...canActivate(redirectUnauthorizedToLogin)
+
   }
 ];
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'ionic-test-project23',
+    appId: '1:595633037709:web:3b1df9db208caa99d41dc4',
+    storageBucket: 'ionic-test-project23.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyB5gPjQyFr4B4vbnh6n8e70aPBHItJFWxI',
+    authDomain: 'ionic-test-project23.firebaseapp.com',
+    messagingSenderId: '595633037709',
+  },
   production: true
 };
